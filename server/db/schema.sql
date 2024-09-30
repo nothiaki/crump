66ea27b57ba883b57a540ded40280f1b9403f5c6
nothiaki <celsohenrique367@gmail.com>
2024-09-29 23:11:09 -0300

    refactor: sql script

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,13 +1,15 @@
 CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
   email varchar(255) NOT NULL UNIQUE,
-  name varchar(255) NOT NULL UNIQUE,
+  name varchar(16) NOT NULL UNIQUE,
+  password varchar(128) NOT NULL,
+  salt varchar(32) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS crumps (
   id SERIAL PRIMARY KEY,
-  content varchar(255) NOT NULL,
+  content varchar(400) NOT NULL,
   likes int NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   user_id INT REFERENCES users(id)
