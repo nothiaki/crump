4a742978a1170c031913f5c262dee236a3bef8a2
nothiaki <celsohenrique367@gmail.com>
2024-10-17 17:39:16 -0300

    feat: logo with title

---
+++ client/src/components/ui/logo/logo-title.tsx
@@ -0,0 +1,12 @@
+import logo from "../../../assets/logo.svg"
+
+export function LogoTitle() {
+  return (
+    <>
+      <div className="text-text-100 flex items-center gap-4">
+        <img src={logo} alt="crump-logo" className="h-12" />
+        <h1 className="text-3xl font-bold">Crump</h1>
+      </div>
+    </>
+  )
+}
