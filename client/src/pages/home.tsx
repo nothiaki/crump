fdbf9869d7a63178e399e98d6872e574f9f78842
nothiaki <celsohenrique367@gmail.com>
2025-01-19 16:05:51 -0300

    refactor: use nest

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -2,7 +2,9 @@ export function Home() {
   return (
     <>
       <div className="h-screen flex items-center justify-center bg-prim-900 text-text-100">
+        <header>logo-create post-account</header>
         <main className="flex flex-col gap-4">
+          a
         </main>
       </div>
     </>
