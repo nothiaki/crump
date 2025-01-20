15c7da2bf6083b97519fab807f056b8fa7818a35
nothiaki <celsohenrique367@gmail.com>
2025-01-19 23:24:56 -0300

    feat: get one route

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  findOne(id: string) {
+    return {
+      id,
+      email: 'jhon@mail.com',
+      name: 'jhon',
+      crumps: 301,
+      createdAt: '2025-01-19 14:30:15.123456+00',
+    };
+  }
+}
