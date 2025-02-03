7dd062c164365526b6f76d0ffb37cb9a19150468
nothiaki <celsohenrique367@gmail.com>
2025-02-02 23:57:46 -0300

    test: crumps service

--- server/src/crumps/crumps.service.spec.ts
+++ server/src/crumps/crumps.service.spec.ts
@@ -1,18 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CrumpsService } from './crumps.service';
+import { CrumpEntity } from './entities/crump.entity';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreateCrumpDto } from './dto/create-crump.dto';
 
 describe('CrumpsService', () => {
-  let service: CrumpsService;
+  let crumpsService: CrumpsService;
+  let crumpsRepository: Repository<CrumpEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CrumpsService],
+      providers: [
+        CrumpsService,
+        {
+          provide: getRepositoryToken(CrumpEntity),
+          useValue: {
+            find: jest.fn(),
+            findOneBy: jest.fn(),
+            remove: jest.fn(),
+          },
+        }
+      ],
     }).compile();
 
-    service = module.get<CrumpsService>(CrumpsService);
+    crumpsService = module.get<CrumpsService>(CrumpsService);
+    crumpsRepository = module.get<Repository<CrumpEntity>>(getRepositoryToken(CrumpEntity));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('Find all crumps', () => {
+    it('should find all crumps', () => {
+      expect(crumpsRepository.find).toHaveBeenCalled();
+    });
+  });
+
+  describe('Delete one crump', () => {
+    it('should delete one crump', () => {
+      expect(crumpsRepository.findOneBy).toHaveBeenCalled();
+      expect(crumpsRepository.remove).toHaveBeenCalled();
+    });
   });
 });
