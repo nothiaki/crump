962d0de4783f1e4da73fc8cedd40cd2146674e17
nothiaki <celsohenrique367@gmail.com>
2025-02-03 23:10:33 -0300

    test: user service test fixed

--- server/src/users/users.service.spec.ts
+++ server/src/users/users.service.spec.ts
@@ -19,9 +19,9 @@ describe('UsersService', () => {
             find: jest.fn(),
             findOneBy: jest.fn(),
             findOne: jest.fn(),
-            create: jest.fn(),
+            create: jest.fn().mockResolvedValue(UserEntity),
             save: jest.fn(),
-            preload: jest.fn(),
+            preload: jest.fn().mockResolvedValue(UserEntity),
           },
         }
       ],
@@ -30,35 +30,53 @@ describe('UsersService', () => {
     usersService = module.get<UsersService>(UsersService);
     usersRepository = module.get<Repository<UserEntity>>(getRepositoryToken(UserEntity));
   });
-
+  
   describe('Find all users', () => {
-    it('should find all users', () => {
+    it('should find all users', async () => {
+      await usersService.findAll();
+
       expect(usersRepository.find).toHaveBeenCalled();
     });
   });
-
+  
   describe('Find one user', () => {
-    it('should find one user', () => {
-      expect(usersRepository.findOneBy).toHaveBeenCalled();
+    it('should find one user', async () => {
+      const id: string = 'cd674539-4a10-42af-b658-2da0a710dc8a';
+      const mockUserPartial = {
+        id,
+        name: 'test',
+        email: 'test@test.test',
+      };
+
+      jest.spyOn(usersRepository, 'findOneBy').mockResolvedValue(mockUserPartial as UserEntity);
+
+      const response = await usersService.findOne(id);
+
+      expect(usersRepository.findOneBy).toHaveBeenCalledWith({ id });
+      expect(response).toBe(mockUserPartial);
     });
   });
-
+  
   describe('Create user', () => {
-    it('should create a user', () => {
+    it('should create a user', async () => {
       const createUserDto: CreateUserDto = {
-        name: 'cooper',
-        email: 'amet@icloud.edu',
-        password: 'MLB79VUM6FJ',
+        name: 'test',
+        email: 'test@test.test',
+        password: 'testTEST',
       };
+
+      await usersService.create(createUserDto);
 
       expect(usersRepository.findOne).toHaveBeenCalledTimes(2);
       expect(usersRepository.create).toHaveBeenCalledWith(createUserDto);
       expect(usersRepository.save).toHaveBeenCalled();
     });
   });
-
+  
   describe('Delete one user', () => {
-    it('should delete one user', () => {
+    it('should delete one user', async () => {
+      await usersService.remove('46421185-f2dc-45c9-af9e-cf98186fa486');
+
       expect(usersRepository.preload).toHaveBeenCalled();
       expect(usersRepository.save).toHaveBeenCalled();
     });
