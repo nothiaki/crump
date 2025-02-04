b4941fd76e5146d461c0ba5adcc42d4cb0925ead
nothiaki <celsohenrique367@gmail.com>
2025-02-03 23:45:42 -0300

    test: crumps service fixed

--- server/src/crumps/crumps.service.spec.ts
+++ server/src/crumps/crumps.service.spec.ts
@@ -3,11 +3,13 @@ import { CrumpsService } from './crumps.service';
 import { CrumpEntity } from './entities/crump.entity';
 import { Repository } from 'typeorm';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { UserEntity } from 'src/users/entities/user.entity';
 import { CreateCrumpDto } from './dto/create-crump.dto';
 
 describe('CrumpsService', () => {
   let crumpsService: CrumpsService;
   let crumpsRepository: Repository<CrumpEntity>;
+  let usersRepository: Repository<UserEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -18,24 +20,66 @@ describe('CrumpsService', () => {
           useValue: {
             find: jest.fn(),
             findOneBy: jest.fn(),
+            create: jest.fn(),
+            save: jest.fn(),
             remove: jest.fn(),
           },
-        }
+        },
+        {
+          provide: getRepositoryToken(UserEntity),
+          useValue: {
+            findOneBy: jest.fn(),
+          },
+        },
       ],
     }).compile();
 
     crumpsService = module.get<CrumpsService>(CrumpsService);
     crumpsRepository = module.get<Repository<CrumpEntity>>(getRepositoryToken(CrumpEntity));
+    usersRepository = module.get<Repository<UserEntity>>(getRepositoryToken(UserEntity));
   });
 
   describe('Find all crumps', () => {
-    it('should find all crumps', () => {
+    it('should find all crumps', async () => {
+      await crumpsService.findAll();
+
       expect(crumpsRepository.find).toHaveBeenCalled();
     });
   });
 
+  describe('Create one crump', () => {
+    it('should create one crump', async () => {
+      const createCrumpDto: CreateCrumpDto = {
+        content: 'test',
+        from: 'tester',
+      };
+
+      jest.spyOn(usersRepository, 'findOneBy').mockResolvedValue({
+        id: '62899595-1fd2-480a-9963-bed113857a96',
+        name: 'tester'
+      } as UserEntity);
+
+      await crumpsService.create(createCrumpDto);
+
+      expect(usersRepository.findOneBy).toHaveBeenCalled();
+      expect(crumpsRepository.create).toHaveBeenCalled();
+      expect(crumpsRepository.save).toHaveBeenCalled();
+    });
+  });
+
   describe('Delete one crump', () => {
-    it('should delete one crump', () => {
+    it('should delete one crump', async () => {
+      const id: number = 1;
+
+      const mockCrumpPartial = {
+        id,
+        content: 'test',
+      };
+
+      jest.spyOn(crumpsRepository, 'findOneBy').mockResolvedValue(mockCrumpPartial as CrumpEntity);
+
+      await crumpsService.remove(id);
+
       expect(crumpsRepository.findOneBy).toHaveBeenCalled();
       expect(crumpsRepository.remove).toHaveBeenCalled();
     });
