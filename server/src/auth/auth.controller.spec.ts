2f90fb8c989092c3637d05738bc2cd3c415d5409
nothiaki <celsohenrique367@gmail.com>
2025-02-07 16:14:37 -0300

    test: auth controller

--- server/src/auth/auth.controller.spec.ts
+++ server/src/auth/auth.controller.spec.ts
@@ -1,20 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { CreateAuthDto } from './dto/create-auth.dto';
 
 describe('AuthController', () => {
-  let controller: AuthController;
+  let authController: AuthController;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: {
+            in: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    authController = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('In', async () => {
+    const createAuthDto: CreateAuthDto = {
+      name: 'test',
+      password: 'testTEST',
+    };
+
+    const res: any = {};
+
+    await authController.create(createAuthDto, res);
+
+    expect(authService.in).toHaveBeenCalledWith(createAuthDto, res);
   });
 });
