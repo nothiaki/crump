f0536a6a570f22ba82939bc206fc2144c003c75b
nothiaki <celsohenrique367@gmail.com>
2025-02-07 15:59:03 -0300

    test: crumps controller

--- server/src/crumps/crumps.controller.spec.ts
+++ server/src/crumps/crumps.controller.spec.ts
@@ -1,20 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CrumpsController } from './crumps.controller';
 import { CrumpsService } from './crumps.service';
+import { CreateCrumpDto } from './dto/create-crump.dto';
 
 describe('CrumpsController', () => {
-  let controller: CrumpsController;
+  let crumpsController: CrumpsController;
+  let crumpsService: CrumpsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CrumpsController],
-      providers: [CrumpsService],
+      providers: [
+        {
+          provide: CrumpsService,
+          useValue: {
+            findAll: jest.fn(),
+            create: jest.fn().mockResolvedValue({ from: {}, }),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<CrumpsController>(CrumpsController);
+    crumpsController = module.get<CrumpsController>(CrumpsController);
+    crumpsService = module.get<CrumpsService>(CrumpsService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('findAll', async () => {
+    await crumpsController.findAll();
+    expect(crumpsService.findAll).toHaveBeenCalled();
   });
+
+  it('create', async () => {
+    const createCrumpDto: CreateCrumpDto = {
+      from: 'test',
+      content: 'random crump',
+    };
+
+    await crumpsController.create(createCrumpDto);
+    expect(crumpsService.create).toHaveBeenCalledWith(createCrumpDto);
+  });
+
+  it('remove', async () => {
+    const id: number = 0;
+
+    await crumpsController.remove(id);
+    expect(crumpsService.remove).toHaveBeenCalledWith(id);
+  });
+
 });
