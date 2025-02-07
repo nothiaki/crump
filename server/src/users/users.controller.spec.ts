6c7077a3b730174ff434666d6f731f3f5c7c4dec
nothiaki <celsohenrique367@gmail.com>
2025-02-06 23:54:34 -0300

    test: users controller

--- server/src/users/users.controller.spec.ts
+++ server/src/users/users.controller.spec.ts
@@ -1,20 +1,59 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 describe('UsersController', () => {
-  let controller: UsersController;
+  let usersController: UsersController;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: {
+            findAll: jest.fn().mockResolvedValue([]),
+            findOne: jest.fn().mockResolvedValue({}),
+            create: jest.fn().mockResolvedValue({}),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<UsersController>(UsersController);
+    usersController = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('findAll', async () => {
+    await usersController.findAll();
+    expect(usersService.findAll).toHaveBeenCalled();
+  });
+
+  it('findOne', async () => {
+    const id: string = '3f351cda-1774-4e0f-9c71-fd4a0eb9ff07';
+
+    await usersController.findOne(id);
+    expect(usersService.findOne).toHaveBeenCalledWith(id);
+  });
+
+  it('create', async () => {
+    const createUserDto: CreateUserDto = {
+      name: 'test',
+      email: 'test@test.test',
+      password: 'testTEST',
+    };
+
+    await usersController.create(createUserDto);
+    expect(usersService.create).toHaveBeenCalledWith(createUserDto);
+  });
+
+  it('remove', async () => {
+    const id: string = '1fd09c9d-7e2a-4d14-bf4f-292db0a61c1e';
+
+    await usersController.remove(id);
+    expect(usersService.remove).toHaveBeenCalledWith(id);
   });
 });
